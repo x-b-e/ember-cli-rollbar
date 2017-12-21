6c5d018b250bf25acce38ee1e4eaa85e44526ed8
Paul Watts <paulcwatts@gmail.com>
2017-12-21 15:40:36 -0800

    Break functionality into utilities
    This breaks the functionality that was in the initializer
    into a set of utilities that can be called from anywhere.
    
    It also removes the previous initializer -- I'll bring it back
    in the next commit.

--- addon/utils/rollbar.js
+++ addon/utils/rollbar.js
@@ -1,5 +1,8 @@
 /* global FastBoot */
 import { assign } from '@ember/polyfills';
+import Ember from "ember";
+
+const { Logger } = Ember;
 
 /**
  * Returns the configuration for the server-side of Rollbar.
@@ -34,4 +37,61 @@ export function getInstance(owner) {
     /* global Rollbar */
     return Rollbar;
   }
+}
+
+// Copied from ember-metal
+function getStack(error) {
+  let { stack, message } = error;
+
+  if (stack && stack.indexOf(message) === -1) {
+    stack = `${message}\n${stack}`;
+  }
+  return stack;
+}
+
+/**
+ * Wraps Ember.onerror with Rollbar. Returns the old value of onerror.
+ */
+export function captureEmberErrors(instance, outputToConsole = true) {
+  let previous = Ember.onerror;
+
+  if (typeof FastBoot !== 'undefined') {
+    // Fastboot's default error handle always prints to the console.
+    Ember.onerror = function(err) {
+      instance.error(err);
+    };
+  } else {
+    // Note this will continue to output the error to the console,
+    // even when it's captured by Rollbar. If you'd prefer to not do this,
+    // set outputToConsole = false.
+    if (outputToConsole) {
+      let origError = Logger.error;
+      Ember.onerror = function(err) {
+        instance.error(err);
+        origError(getStack(err));
+      };
+    } else {
+      Ember.onerror = function(err) {
+        instance.error(err);
+      };
+    }
+  }
+  return previous;
+}
+
+function wrapLogger(instance, fn, method) {
+  return function() {
+    instance[method](...arguments);
+    fn.apply(this, arguments);
+  };
+}
+
+/**
+ * Wraps Ember.Logger methods in Rollbar.
+ */
+export function captureEmberLogger(instance) {
+  Logger.error = wrapLogger(instance, Logger.error, 'error');
+  Logger.warn = wrapLogger(instance, Logger.warn, 'warning');
+  Logger.info = wrapLogger(instance, Logger.info, 'info');
+  Logger.debug = wrapLogger(instance, Logger.debug, 'debug');
 }
--- tests/unit/utils/rollbar-test.js
+++ tests/unit/utils/rollbar-test.js
@@ -1,9 +1,25 @@
-import { getServerConfig, getInstance } from 'dummy/utils/rollbar';
+import { getServerConfig, getInstance, captureEmberErrors, captureEmberLogger } from 'dummy/utils/rollbar';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import Ember from 'ember';
+import sinonTest from 'ember-sinon-qunit/test-support/test';
+
+const { Logger } = Ember;
+
+class MockRollbar {
+  info() {}
+  error() {}
+}
 
 module('Unit | Utility | rollbar', function(hooks) {
   setupTest(hooks);
+
+  hooks.beforeEach(function() {
+    Ember.onerror = undefined;
+  });
+  hooks.afterEach(function() {
+    Ember.onerror = undefined;
+  });
 
   test('getServerConfig', function(assert) {
     let result = getServerConfig(this.owner);
@@ -22,5 +38,48 @@ module('Unit | Utility | rollbar', function(hooks) {
     assert.ok(rollbar.info, 'info exists');
     assert.ok(rollbar.error, 'error exists');
     assert.ok(rollbar.configure, 'configure exists');
+  });
+
+  sinonTest('captureEmberErrors', function(assert) {
+    let rollbar = new MockRollbar();
+    let mock = this.mock(rollbar);
+    mock.expects('error');
+
+    let log = this.spy(Logger, 'error');
+
+    let result = captureEmberErrors(rollbar);
+    Ember.onerror(new Error('hello'));
+
+    assert.ok(log.calledOnce);
+    assert.strictEqual(result, undefined);
+    mock.verify();
+  });
+
+  sinonTest('captureEmberErrors no console output', function(assert) {
+    let rollbar = new MockRollbar();
+    let mock = this.mock(rollbar);
+    mock.expects('error');
+
+    let log = this.spy(Logger, 'error');
+
+    captureEmberErrors(rollbar, false);
+    Ember.onerror(new Error('hello'));
+
+    assert.notOk(log.called);
+    mock.verify();
+  });
+
+  sinonTest('captureEmberLogger', function(assert) {
+    let rollbar = new MockRollbar();
+    let mock = this.mock(rollbar);
+    mock.expects('info');
+
+    let log = this.spy(Logger, 'info');
+
+    captureEmberLogger(rollbar);
+    Ember.Logger.info('my info message');
+
+    assert.ok(log.called);
+    mock.verify();
   });
 });
